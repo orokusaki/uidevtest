f33a42cee8771a19304453b867aa4138cd29f49c
Michael Angeletti <michael@angelettigroup.com>
2012-09-28 14:22:22 -0400

    Rudimentarily finished with story view, time for list

--- src/js/common.js
+++ src/js/common.js
@@ -1,11 +1,11 @@
 jQuery(function($){
 	$.getJSON('../js/uidevtest-data.js', function(data){
-		// Attach the feed to the body so that the data can be
-		// quickly used later without the need for more requests
+		// Attach the feed to the body so that the data can be quickly used
+		// later without the need for more requests
 		$('body').data('stories', data.objects);
 
-		// If a story was requested, display it, otherwise display
-		// the list of the stories in the lively data feed
+		// If a story was requested, display it, otherwise display the list of
+		// the stories in the lively data feed
 		var story_idx = get_story_idx();
 		if(story_idx !== null){
 			display_story(story_idx);
@@ -15,14 +15,95 @@ jQuery(function($){
 	});
 
 	function display_story(idx){
-		// Displays a story provided an integer representing the
-		// story's 0-based index among the stories data feed
+		// Displays a story provided an integer representing the story's
+		// 0-based index among the stories data feed, else displays a 404
+
+		// Remove existing content
+		clear_story();
+
+		// Get the story
 		var story = get_story(idx);
+
 		if(story === null){
 			// Display 404
+			$('.story .info .title').text('Error 404');
+			$('.story .info .meta').text('(story not found)');
+			$('<img>')
+				.attr('src', 'http://placekitten.com/260/350')
+				.attr('alt', 'Meow :(')
+				.attr('class', 'loads')
+				.prependTo($('.story .left .photo'));
 		} else {
-			// Kapa-Chow!
+			// Display story
+			$('.story .info .title').text(story.title);
+			var pub_date = ap_date(story.pub_date);
+			var updated = ap_date(story.updated);
+			$('.story .info .meta').text('Updated ' + pub_date.toLocaleString() + ' | Updated ' + updated.toLocaleString());
+			var caption = story.lead_photo_caption;
+			if(caption === null){
+				caption = story.summary;
+			}
+
+			// Main photo and credit
+			$('<img>')
+				.attr('src', story.lead_photo_image_url)
+				.attr('alt', caption)
+				.attr('class', 'loads')
+				.prependTo($('.story .left .photo'));
+			$('.story .left .photo figcaption').text(story.lead_photo_credit);
+
+			// Caption
+			$('.story .left .photo_desc').text(caption);
+
+			// Right content / author
+			$('.story .right .author').text('By ');
+			$('<strong>')
+				.text(story.author.join(', '))
+				.appendTo($('.story .right .author'));
+			$('.story .right .content').html(story.story);
+
+			// Crumb trail
+			var crumbs = $('.crumbs ul');
+			crumbs.find('.dynamic').remove();
+			$.each(story.categories_name, function(idx, category_name){
+				$('<strong>').html('&gt;').appendTo($('<li>').appendTo(crumbs));
+				var crumb = $('<li>')
+					.attr('class', 'dynamic')
+					.appendTo(crumbs)
+				$('<a>')
+					.attr('href', '#')
+					.text(category_name)
+					.appendTo(crumb)
+			});
+			$('<strong>')
+				.text(story.title)
+				.appendTo($('<li>').appendTo(crumbs));
 		}
+	}
+
+	function ap_date(iso_datetime){
+		// Formats the provided ISO date string into AP format
+		var d = Date.parse(iso_datetime);
+		var d = new Date(d);
+		return d.getHours12()
+			+ ':'
+			+ d.getMinutes()
+			+ ' '
+			+ d.getAPMeridiem()
+			+ ' '
+			+ d.getDayName()
+			+ ', '
+			+ d.getMonthAbbr()
+			+ '. '
+			+ d.getDate()
+			+ ', '
+			+ d.getFullYear();
+	}
+
+	function clear_story(){
+		// Removes story content
+		$('.story .loads:not(img)').html('');
+		$('.story img.loads').remove();
 	}
 
 	function display_list(){
@@ -33,8 +114,8 @@ jQuery(function($){
 	}
 
 	function get_story(idx){
-		// Returns a story from the stories data feed for the given
-		// index , or `null`, if one doesn't exist for the index
+		// Returns a story from the stories data feed for the given index, or
+		// `null`, if one doesn't exist for the index
 		var story = $('body').data('stories')[idx];
 		if(typeof(story) == 'undefined')
 			return null;
@@ -43,12 +124,12 @@ jQuery(function($){
 });
 
 function get_story_idx(){
-	// Returns a 0-based story index, based on a "story" URL param,
-	// or returns `null`, if story index not applicable
+	// Returns a 0-based story index, based on a "story" URL param, or returns
+	// `null`, if the proper pattern is not provided in the "story" param
 	var prefix = 'sto';
 	var param = getURLParameter('story');
-	// Create a regexp that allows 2 digits for the story index,
-	// since that's what the `sto01`
+	// Create a regexp that allows 2 digits for the story index, since that's
+	// what the `sto01` example in the instructions indicates (leading zero)
 	var regexp = new RegExp('^' + prefix + '\\d{2}$');
 	if(param !== null && param.match(regexp)){
 		var idx = parseInt(param.slice(prefix.length));
@@ -62,3 +143,34 @@ function getURLParameter(name){
 	// A "universal" URL param function from SO
 	return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
+
+// Some datetime-fu so that I don't have to use jQuery UI's half-backed date
+// picker, or do some ugly-inline date time formatting - inspired by SO
+(function($){
+	var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+	// Abbr of month name
+	Date.prototype.getMonthAbbr = function(){
+		return months[this.getMonth()];
+	};
+
+	// Name of day of week
+	Date.prototype.getDayName = function(){
+		return days[this.getDay()];
+	};
+
+	// AP-formatted a.m. / p.m.
+	Date.prototype.getAPMeridiem = function(){
+		if(this.getHours() < 12)
+			return 'a.m.';
+		return 'p.m.';
+	};
+
+	// 12-hour-based hours
+	Date.prototype.getHours12 = function(){
+		if(this.getHours() < 13)
+			return this.getHours();
+		return this.getHours() - 12;
+	}
+})();
