43f70e191568a32c8ccb2dfa8ab68a9b8751d7a9
Michael Angeletti <michael@angelettigroup.com>
2012-09-28 17:21:05 -0400

    Working on list view

--- src/js/common.js
+++ src/js/common.js
@@ -18,6 +18,9 @@ jQuery(function($){
 		// Displays a story provided an integer representing the story's
 		// 0-based index among the stories data feed, else displays a 404
 
+		$('.story').toggle();
+		$('.list_feed').toggle();
+
 		// Remove existing content
 		clear_story();
 
@@ -35,14 +38,21 @@ jQuery(function($){
 				.prependTo($('.story .left .photo'));
 		} else {
 			// Display story
+
+			// Headline
 			$('.story .info .title').text(story.title);
+
+			// Publish and Update dates
 			var pub_date = ap_date(story.pub_date);
 			var updated = ap_date(story.updated);
 			$('.story .info .meta').text('Updated: ' + updated.toLocaleString() + ' | Posted: ' + pub_date.toLocaleString());
+
+			// Caption (uses summary when caption is null - sneaky, sneaky)
 			var caption = story.lead_photo_caption;
 			if(caption === null){
 				caption = story.summary;
 			}
+			$('.story .left .photo_desc').text(caption);
 
 			// Main photo and credit
 			$('<img>')
@@ -52,39 +62,28 @@ jQuery(function($){
 				.prependTo($('.story .left .photo'));
 			$('.story .left .photo figcaption').text(story.lead_photo_credit);
 
-			// Caption
-			$('.story .left .photo_desc').text(caption);
-
 			// Right content / author
 			$('.story .right .author').text('By ');
 			$('<strong>')
 				.text(story.author.join(', '))
 				.appendTo($('.story .right .author'));
 			$('.story .right .content').html(story.story);
-
-			// Crumb trail
-			var crumbs = $('.crumbs ul');
-			crumbs.find('.dynamic').remove();
-			$.each(story.categories_name, function(idx, category_name){
-				$('<strong>').html('&gt;').appendTo($('<li>').appendTo(crumbs));
-				var crumb = $('<li>')
-					.attr('class', 'dynamic')
-					.appendTo(crumbs)
-				$('<a>')
-					.attr('href', '#')
-					.text(category_name)
-					.appendTo(crumb)
-			});
-			// Current story crumb
-			$('<strong>').html('&gt;').appendTo($('<li>').appendTo(crumbs));
-			var crumb_title = story.title;
-			if(crumb_title.length > 30){
-				crumb_title = crumb_title.slice(0, 30) + '&hellip;';
-			}
-			$('<strong>')
-				.html(crumb_title)
-				.appendTo($('<li>').appendTo(crumbs));
 		}
+	}
+
+	function clear_story(){
+		// Removes story content
+		$('.story .loads:not(img)').html('');
+		$('.story img.loads').remove();
+	}
+
+	function get_story(idx){
+		// Returns a story from the stories data feed for the given index, or
+		// `null`, if one doesn't exist for the index
+		var story = $('body').data('stories')[idx];
+		if(typeof(story) == 'undefined')
+			return null;
+		return story;
 	}
 
 	function ap_date(iso_datetime){
@@ -93,7 +92,7 @@ jQuery(function($){
 		var d = new Date(d);
 		return d.getHours12()
 			+ ':'
-			+ d.getMinutes()
+			+ d.getFullMinutes()
 			+ ' '
 			+ d.getAPMeridiem()
 			+ ' '
@@ -106,26 +105,14 @@ jQuery(function($){
 			+ d.getFullYear();
 	}
 
-	function clear_story(){
-		// Removes story content
-		$('.story .loads:not(img)').html('');
-		$('.story img.loads').remove();
-	}
-
 	function display_list(){
 		// Displays a list of stories from the stories data feed
+		$('.story').toggle();
+		$('.list_feed').toggle();
+		clear_story();
 		$.each($('body').data('stories'), function(idx, story){
-			// Go to town!
-		});
-	}
 
-	function get_story(idx){
-		// Returns a story from the stories data feed for the given index, or
-		// `null`, if one doesn't exist for the index
-		var story = $('body').data('stories')[idx];
-		if(typeof(story) == 'undefined')
-			return null;
-		return story;
+		});
 	}
 });
 
@@ -152,7 +139,7 @@ function getURLParameter(name){
 
 // Some datetime-fu so that I don't have to use jQuery UI's half-backed date
 // picker, or do some ugly-inline date time formatting - inspired by SO
-(function($){
+(function(){
 	var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
@@ -178,5 +165,14 @@ function getURLParameter(name){
 		if(this.getHours() < 13)
 			return this.getHours();
 		return this.getHours() - 12;
-	}
+	};
+
+	// 2-digit minutes
+	Date.prototype.getFullMinutes = function(){
+		var m = this.getMinutes()
+		if(m < 10){
+			return '0' + m;
+		}
+		return m;
+	};
 })();
